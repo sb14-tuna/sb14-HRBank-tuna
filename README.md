70974215fe856eabcea24780af91f1e85e8562a5
joonhee <parkes1801@naver.com>
2026-09-07 23:54:12 +0900

    chore: 프로젝트 초기 설정

---
+++ README.md
@@ -0,0 +1,12 @@
+
+### 실행 방법
+1. 프로젝트 클론받기
+2. 터미널에 docker compose up -d 실행 
+3. 인텔리제이 환경변수 설정
+   - .env 생성 및 작성 - 팀장한테 요청
+   - Run -> Edit Configuration -> HrbankApplication -> Environment variables에 값 입력
+4. 앱 동작 확인
+
+
+### Flyway 마이그레이션 SQL 파일
+- 어케할지
---
+++ src/main/java/com/sb14/hrbank/HrbankApplication.java
@@ -0,0 +1,13 @@
+package com.sb14.hrbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrbankApplication.class, args);
+	}
+
+}
