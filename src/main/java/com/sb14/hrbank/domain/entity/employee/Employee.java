2c2cee77696be265f581a250e21bd26b8bc17280
Minjae Kim <137mjkim@gmail.com>
2026-09-09 10:20:29 +0900

    feat: HRB-27 직원 정보 엔티티 구현
    * 사원번호는 무작위 10자리 숫자, Id처럼 객체 생성 때 생성됨. 수정 불가능

--- src/main/java/com/sb14/hrbank/domain/entity/employee/Employee.java
+++ src/main/java/com/sb14/hrbank/domain/entity/employee/Employee.java
@@ -3,56 +3,98 @@ package com.sb14.hrbank.domain.entity.employee;
 import com.sb14.hrbank.domain.entity.department.Department;
 import com.sb14.hrbank.domain.entity.employeehistory.EmployeeHistory;
 import com.sb14.hrbank.domain.entity.file.File;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
+import java.util.concurrent.ThreadLocalRandom;
+
+import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder(access = AccessLevel.PRIVATE)
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Entity
 @Table(name = "employees")
 public class Employee {
-    @Id @GeneratedValue(strategy = GenerationType.SEQUENCE) //
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "employee_seq")
+    @SequenceGenerator(
+            name = "employee_seq",
+            sequenceName = "employee_seq",
+            allocationSize = 1
+    )
     @Column(name = "employee_id")
     Long id;
 
-    @Column(name = "employee_name")
+    @Column(name = "employee_name", nullable = false)
     String name;
 
-    @Column(name = "employee_email")
+    @Column(
+            name = "employee_email",
+            unique = true,
+            nullable = false
+    )
     String email;
 
-    @Column(name = "employee_number")
-    String employeeNumber;
+    // 랜덤 10자, 자동부여, 수정 불가능
+    @Column(
+            name = "employee_number",
+            unique = true,
+            nullable = false,
+            updatable = false
+    )
+    String employeeNumber = String.valueOf(
+            ThreadLocalRandom.current()
+                    .nextLong(1_000_000_000L, 10_000_000_000L)
+    );
 
-    @Column(name = "employee_position")
+    @Column(
+            name = "employee_position",
+            nullable = false,
+            length = 10
+    )
     String position;
 
-    @Column(name = "employee_hiredate")
+    @Column(name = "employee_hiredate", nullable = false)
     LocalDate hireDate;
 
-
-    //EmployeeStatus status;        // todo : 이넘 만들기
+    @Enumerated(EnumType.STRING)
+    @Column(name = "employee_status", nullable = false)
+    EmployeeStatus status;
 
     @ManyToOne
-    @JoinColumn(name = "department_id")
-    Department department;
+    @JoinColumn(name = "department_id", nullable = false)
+    Department departmentId;
 
     @OneToOne
-    @JoinColumn(name = "file_id")
-    File profileImage;
+    @JoinColumn(name = "file_id", nullable = true)
+    File profileImageId;
+
+
+    public static Employee init(
+            String name,
+            String email,
+            String position,
+            LocalDate hireDate,
+            EmployeeStatus status,
+            Department departmentId,
+            File profileImageId
+    ) {
+        return Employee.builder()
+                .name(name)
+                .email(email)
+                .position(position)
+                .hireDate(hireDate)
+                .status(status)
+                .departmentId(departmentId)
+                .profileImageId(profileImageId)
+                .build();
+    }
 }
