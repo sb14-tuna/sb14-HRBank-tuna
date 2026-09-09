212e098050cb79c72bcdb289288a5b6bb1eb018d
Minjae Kim <137mjkim@gmail.com>
2026-09-09 17:38:40 +0900

    fix: PR 피드백 반영
    * AllArgsConstructor를 private 접근으로 수정
    * NoArgsConstructor를 protected 접근으로 수정 - Jpa/Hibernate는 사용할 수 있되 무분별 객체 생성 제한한다

--- src/main/java/com/sb14/hrbank/domain/entity/employee/Employee.java
+++ src/main/java/com/sb14/hrbank/domain/entity/employee/Employee.java
@@ -9,8 +9,8 @@ import lombok.experimental.FieldDefaults;
 import java.time.LocalDate;
 import java.util.concurrent.ThreadLocalRandom;
 
-@AllArgsConstructor
-@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Builder(access = AccessLevel.PRIVATE)
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Entity
