a9f06a35c7687177293ac4e2b0440fc88ab406dd
배원기 <wongi0824@gmail.com>
2026-09-10 14:47:54 +0900

    feat: 부서 등록/수정/삭제

---
+++ src/main/java/com/sb14/hrbank/domain/service/department/DepartmentServiceImpl.java
@@ -0,0 +1,11 @@
+package com.sb14.hrbank.domain.service.department;
+
+import com.sb14.hrbank.domain.repository.IDepartmentRepository;
+import jakarta.transaction.Transactional;
+
+public class DepartmentService implements IDepartmentService{
+    private final IDepartmentRepository departmentRepository;
+
+    @Override
+    @Transactional
+}
---
+++ src/main/java/com/sb14/hrbank/domain/service/department/IDepartmentService.java
@@ -0,0 +1,4 @@
+package com.sb14.hrbank.domain.service.department;
+
+public interface IDepartmentService {
+}
