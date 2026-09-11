216edf61be434e3768cc73aab68cc5d211890946
joonhee <parkes1801@naver.com>
2026-09-11 14:25:27 +0900

    refactor: 예외 필드 변경

--- src/main/java/com/sb14/hrbank/domain/exception/HrBankExceptionType.java
+++ src/main/java/com/sb14/hrbank/domain/exception/HrBankExceptionType.java
@@ -1,10 +1,9 @@
-package com.sb14.hrbank.web.exception;
+package com.sb14.hrbank.domain.exception;
 
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import lombok.extern.slf4j.Slf4j;
 import org.slf4j.event.Level;
 import org.springframework.http.HttpStatus;
 
@@ -16,10 +15,9 @@ public enum HrBankExceptionType {
     TMP_EXCEPTION(
             Level.ERROR,
             HttpStatus.BAD_REQUEST,
-            "bad request 입니다"
+            "bad request 입니다",
+        "bad"
     ),
-
-
 
 
 
@@ -28,21 +26,33 @@ public enum HrBankExceptionType {
     FILE_NOT_FOUND(
         Level.ERROR,
         HttpStatus.NOT_FOUND,
-        "해당 파일을 찾을 수 없습니다."
+        "해당 파일을 찾을 수 없습니다.",
+        "bad"
     ),
     FILE_LOAD_FAILED(
         Level.ERROR,
         HttpStatus.INTERNAL_SERVER_ERROR,
         "키 값으로 물리파일을 불러오는 것이 실패하였습니다."
+        ,"bad"
     ),
     FILE_KEY_INVALID(
         Level.ERROR,
         HttpStatus.BAD_REQUEST,
         "파일 저장 키가 유효하지 않습니다."
+        ,"bad"
+    ),
+
+
+    // 백업관련
+    BACKUP_STATE_INVALID_CHANGE(
+        Level.ERROR,
+        HttpStatus.BAD_REQUEST,
+        "백업 이력 상태를 변경할 수 없습니다."
+        ,"bad"
     );
 
     Level logLevel;
     HttpStatus status;
-    String response;
-
+    String message;
+    String details;
 }
