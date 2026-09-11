45d234bc4767535597cfd0c6cd25b3ebb00beb37
배원기 <wongi0824@gmail.com>
2026-09-11 18:53:25 +0900

    부서 서비스 변경

--- src/main/java/com/sb14/hrbank/domain/service/department/DepartmentServiceImpl.java
+++ src/main/java/com/sb14/hrbank/domain/service/department/DepartmentServiceImpl.java
@@ -1,8 +1,12 @@
 package com.sb14.hrbank.domain.service.department;
 
+
 import com.sb14.hrbank.domain.entity.department.Department;
-import com.sb14.hrbank.domain.repository.IDepartmentRepository;
-import com.sb14.hrbank.domain.repository.IEmployeeRepository;
+import com.sb14.hrbank.domain.repository.DepartmentRepository;
+import com.sb14.hrbank.domain.repository.EmployeeRepository;
+import com.sb14.hrbank.web.controller.dto.DepartmentCreateRequest;
+import com.sb14.hrbank.web.controller.dto.DepartmentDto;
+import com.sb14.hrbank.web.controller.dto.DepartmentUpdateRequest;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,9 +16,9 @@ import java.util.NoSuchElementException;
 @Service
 @RequiredArgsConstructor
 @Transactional
-public class DepartmentServiceImpl implements IDepartmentService{
-    private final IDepartmentRepository departmentRepository;
-    private final IEmployeeRepository employeeRepository;
+public class DepartmentServiceImpl implements DepartmentService {
+    private final DepartmentRepository departmentRepository;
+    private final EmployeeRepository employeeRepository;
 
     @Override
     @Transactional
@@ -33,25 +37,18 @@ public class DepartmentServiceImpl implements IDepartmentService{
     @Override
     @Transactional
     public DepartmentDto updateDepartment(Long departmentId, DepartmentUpdateRequest updateRequest) {
-        Department department = departmentRepository.findById(departmentId)
-                .orElseThrow(() -> new NoSuchElementException("존재하지 않는 부서: " + departmentId));
+        Department department = departmentRepository.findById(departmentId).orElseThrow(() -> new NoSuchElementException("존재 하지 않는 부서: " + departmentId));
 
-        if (!department.getName().equals(updateRequest.getName())
-        && departmentRepository.existsByName(updateRequest.getName())) {
+        if (!department.getName().equals(updateRequest.getName()) && departmentRepository.existsByName(updateRequest.getName())) {
             throw new IllegalArgumentException("이미 존재하는 부서명입니다.: " + updateRequest.getName());
         }
-        department.updateDepartmentInfo(updateRequest.getName(), updateRequest.getDescription(), updateRequest.getEstablishedDate());
+        department.updateDepartmentInfo(updateRequest.getName(), updateRequest.getDescription(),updateRequest.getEstablishedDate());
         return DepartmentDto.from(department);
     }
     @Override
     @Transactional
     public void deleteDepartment(Long departmentId) {
-        Department department = departmentRepository.findById(departmentId)
-                .orElseThrow(() -> new NoSuchElementException("존재하지 않는 부서: " + departmentId));
+        Department department = departmentRepository.findById(departmentId).orElseThrow(() -> new NoSuchElementException("존재하지 않는 부서: " + departmentId));
 
-        if (employeeRepository.existsByDepartmentId(departmentId)) {
-            throw new IllegalArgumentException("소속된 직원이 있어서 삭제할 수 없습니다.: " + departmentId);
-        }
-        departmentRepository.delete(department);
     }
 }
